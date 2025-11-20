e534b7a40f6f1f3578706b17a9a246049767a30c
Pugazhenthi <pugalpugal731@gmail.com>
2025-11-20 22:09:52 +0530

    Refactor script1.js for clarity and organization

--- Digital Calculator/script1.js
+++ Digital Calculator/script1.js
@@ -14,7 +14,7 @@ let initialvalue = "";
 // This function is handle the operators
 function Handle_Operator(operator) {
   let len = initialvalue.length - 1;
-  if (/[+-*/%]/.test(initialvalue[len])) {
+  if (/[+\-*/%]/.test(initialvalue[len])) {
     initialvalue = initialvalue.slice(0, -1);
   }
   Handle_Button(operator);
@@ -113,6 +113,7 @@ back_button.addEventListener("click", Back_button);
 clear_button.addEventListener("click", Clear_Button);
 equal.addEventListener("click", calculate);
 toggle.addEventListener("click", Theme_toggle);
+
 
 
 
